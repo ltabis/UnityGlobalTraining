75faba763b6a3f17c3c8a6f855b561a6c5623d75
Arthur <arthur.lang@epitech.eu>
2019-12-20 16:16:44 +0100

    add timer

---
+++ Assets/timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    /* class for timer behaviour */
+
+    private Text clockTest;
+    private float startTime = 300;
+    // Start is called before the first frame update
+    void Start()
+    { }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentTime = startTime - Time.time;
+        string min = ((int)currentTime / 60).ToString();
+        string sec = (currentTime % 60).ToString("f0");
+        GameObject.Find("Clock").GetComponent<UnityEngine.UI.Text>().text = min + " : " + sec;
+    }
+}
