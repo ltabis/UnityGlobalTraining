447ca4573f71823b8e51786f87af34a4c63d583b
ltabis <tab.rysak70@gmail.com>
2019-12-25 14:44:24 +0100

    :sparkles: Added a basic player boost implementation.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,53 +4,73 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Movement
     public float rotationSpeed = 360;
     public float movementSpeed = 20f;
 
-    // Not yet used
-    public float boostSpeed = 15f;
-    public float boostDuration = 2f;
+    // Boost
+    public float boostFactor = 2f;
+    public float boostDuration = 1f;
+    public float boostCoolDown = 2f;
+
+    private float boostUseTime = 0f;
+    private float boostCoolDownTime = 0f;
+    private bool boostActivated = false;
 
     // Update is called once per frame
     private void Update()
     {
-        if (Input.GetButtonDown("boost"))
+        // Checking if the boost button as been pressed
+        if (boostCoolDownTime <= 0f && (Input.GetButton("boost") || boostActivated))
         {
-            BoostThrust();
+            UseBoost(movementSpeed * boostFactor);
         }
         else
         {
-            RegularThrust();
+            Thrust(movementSpeed);
+            boostCoolDownTime -= boostCoolDownTime > 0 ? Time.deltaTime : 0;
         }
-        RotateObject();
+
+        // Rotating the object
+        RotateObject(rotationSpeed);
     }
 
     // Thrust the object forward
-    private void RegularThrust()
+    private void Thrust(float speed)
     {
         // Getting the velocity vector
-        Vector3 velocity = new Vector3(0, Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime, 0);
+        Vector3 velocity = new Vector3(0, Input.GetAxis("Vertical") * speed * Time.deltaTime, 0);
 
         // Thrusting in the direction of the front of the object
         transform.position += transform.rotation * velocity;
     }
 
-    // Boosts the ship forward
-    private void BoostThrust()
-    {
-        // Not yet implemented
-    }
-
     // Rotate the object on the z axis
-    private void RotateObject()
+    private void RotateObject(float speed)
     {
         // Getting z coordinate rotation
         float z = transform.rotation.eulerAngles.z;
 
         // Substracting horizontal abcissia
-        z -= Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        z -= Input.GetAxis("Horizontal") * speed * Time.deltaTime;
 
         // Updating transform
         transform.rotation = Quaternion.Euler(0, 0, z);
+    }
+
+    private void UseBoost(float thrustPower)
+    {
+        if (boostUseTime < boostDuration)
+        {
+            Thrust(thrustPower);
+            boostUseTime += Time.deltaTime;
+            boostActivated = true;
+        }
+        else
+        {
+            boostActivated = false;
+            boostUseTime = 0f;
+            boostCoolDownTime = boostCoolDown;
+        }
     }
 }
