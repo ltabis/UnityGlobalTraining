212379f70c3c37695f9a328f8f00ceeaf1b21ecc
matteovol <matteo.volpi@epitech.eu>
2020-01-05 17:05:28 +0100

    Enemy
    Enemy go toward the player

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -1,38 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehavior : MonoBehaviour
 {
-    float range = 10f;
-    //float speed = 0.1f;
-    Transform target;
+    public float speed = 0.1f;
+    private Transform target;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        target = GameObject.FindGameObjectWithTag("Player").transform;
-    }
+    void Start() {}
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 d = new Vector3(
-            System.Math.Abs(transform.position.x - target.position.x),
-            System.Math.Abs(transform.position.y - target.position.y),
-            System.Math.Abs(transform.position.z - target.position.z)
-        );
-
-        Debug.Log(d.ToString());
-        Debug.Log(target.ToString());
-        if (d.x < range || d.y < range) {
-            transform.Translate(new Vector3(1, 1));
-        }
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, range);
+        target = GameObject.Find("Body").transform;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed);
     }
 }
