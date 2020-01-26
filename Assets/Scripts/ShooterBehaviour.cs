e76f037c5d31cbd7c15a5b70ff480b76268913e7
Arthur <arthur.lang@epitech.eu>
2020-01-26 14:22:04 +0100

    add shooterBehaviour

---
+++ Assets/Scripts/ShooterBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterBehaviour : MonoBehaviour
+{
+    [SerializeField] private float time = 0.0f;
+    [SerializeField] private float gap = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+           
+    }
+}
