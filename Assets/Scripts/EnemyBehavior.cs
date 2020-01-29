2305acd756f4fb594a6ba4caa251dd49f6f1d47f
Arthur <arthur.lang@epitech.eu>
2020-01-29 10:30:49 +0100

    improve ennemy behaviour

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -9,7 +9,10 @@ public class EnemyBehavior : MonoBehaviour, IEnemy
     private Transform target;
 
     // Start is called before the first frame update
-    private void Start() {}
+    private void Start()
+    {
+        speed = Random.Range(0.05f, 0.2f);
+    }
 
     // Update is called once per frame
     private void Update()
